717642a42a6a949dec101ed465bc36c63fc74223
Paul Slomkowski <paulslomkowski@yahoo.com>
2025-01-20 22:33:52 -0500

    context root nfl fix

--- src/main/java/com/pas/spring/AppConfig.java
+++ src/main/java/com/pas/spring/AppConfig.java
@@ -1,5 +1,8 @@
 package com.pas.spring;
 
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.boot.web.servlet.server.ConfigurableServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ComponentScan.Filter;
 import org.springframework.context.annotation.Configuration;
@@ -7,4 +10,11 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @ComponentScan(basePackages = {"com.pas.spring"},
 	excludeFilters={@Filter(org.springframework.stereotype.Controller.class)})
-public class AppConfig {}
+public class AppConfig
+{
+	@Bean
+	public WebServerFactoryCustomizer<ConfigurableServletWebServerFactory> webServerFactoryCustomizer() 
+	{
+	   return factory -> factory.setContextPath("/nfl");
+	}
+}
